d3e41a2c437b43aa67257c11717e6092d66f6201
Esraa <esraasherif1042@gmail.com>
2025-02-06 08:47:51 +0200

    Model

---
+++ MVC_ITI/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace MVC_ITI.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public float Price { get; set; }
+        public string Image { get; set; }
+
+
+    }
+}
---
+++ MVC_ITI/Models/ProductBL.cs
@@ -0,0 +1,24 @@
+﻿namespace MVC_ITI.Models
+{
+    public class ProductBL
+    {
+        List<Product> products;
+        public ProductBL()
+        {
+            products = new List<Product>();
+            products.Add(new Product() { Id = 1, Name = "Pineapple", Price = 50,Description="Fruit",Image="2.jpg" }) ;
+            products.Add(new Product() { Id = 2, Name = "apple", Price = 25, Description = "Fruit", Image = "1.jpg" });
+            products.Add(new Product() { Id = 1, Name = "orange", Price = 75, Description = "Fruit", Image = "3.jpg" });
+        }
+
+        public List<Product>GetALL()
+        {
+            return products;
+        }
+
+        public Product GetById(int id)
+        {
+            return products.FirstOrDefault(p => p.Id == id);
+        }
+    }
+}
