1a670ce91ecdd9488cd51651c08b2492d3b546bc
Esraa <esraasherif1042@gmail.com>
2025-02-06 10:32:40 +0200

    GetAll+Details

---
+++ MVC_ITI/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MVC_ITI.Models;
+
+namespace MVC_ITI.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            ProductBL productBL = new ProductBL();
+            List<Product> products = productBL.GetALL();
+            return View("Index",products);
+        }
+        public IActionResult Details(int id)
+        {
+            ProductBL productBL = new ProductBL();
+            Product products = productBL.GetById(id);
+
+            if (products == null)
+            {
+                return RedirectToAction("Index"); 
+            }
+            return View("Details", products);
+        }
+    }
+}
--- MVC_ITI/Models/ProductBL.cs
+++ MVC_ITI/Models/ProductBL.cs
@@ -8,7 +8,7 @@
             products = new List<Product>();
             products.Add(new Product() { Id = 1, Name = "Pineapple", Price = 50,Description="Fruit",Image="2.jpg" }) ;
             products.Add(new Product() { Id = 2, Name = "apple", Price = 25, Description = "Fruit", Image = "1.jpg" });
-            products.Add(new Product() { Id = 1, Name = "orange", Price = 75, Description = "Fruit", Image = "3.jpg" });
+            products.Add(new Product() { Id = 3, Name = "orange", Price = 75, Description = "Fruit", Image = "3.jpg" });
         }
 
         public List<Product>GetALL()
